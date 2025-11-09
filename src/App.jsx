4d09dc7e099cefa8b570ed06b9dd599d874be2d7
Khushi Srivastava <sweetkhushi2000@gmail.com>
2025-11-10 01:38:04 +0530

    added conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,38 @@
 import { useState } from "react";
 import "./App.css";
 import TouristPlaces from "./Components/TouristPlaces";
-const places = [
+const touristPlaces = [
   {
-    id: 1,
-    name: "Taj Mahal",
-    description:
-      "Iconic white marble mausoleum and one of the seven wonders of the world, located in Agra.",
-    imgUrl:
-      "https://images.unsplash.com/photo-1548013146-72479768bada?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dGFqbWFoYWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=700",
-    location: "Agra, Uttar Pradesh",
-    isUNESCO: true,
-    bestVisitSeason: [
-      "October",
-      "November",
-      "December",
-      "January",
-      "February",
-      "March",
-    ],
-    entryFee: 50, // in INR
+    placeName: "Goa",
+    desc: "Famous for its beaches, nightlife, and Portuguese heritage.",
+    img: "https://images.unsplash.com/photo-1614082242765-7c98ca0f3df3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z29hfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=500",
+    whenToVisit: "Winter",
+    ratings: 4.5,
+    price: 15000,
   },
   {
-    id: 2,
-    name: "Hampi",
-    description:
-      "Ancient city ruins with stunning boulders, temples, and UNESCO World Heritage status in Karnataka.",
-    imgUrl:
-      "https://images.unsplash.com/photo-1616606484004-5ef3cc46e39d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFtcGl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=700",
-    location: "Karnataka",
-    isUNESCO: true,
-    bestVisitSeason: ["November", "December", "January", "February"],
-    entryFee: 40,
+    placeName: "Manali",
+    desc: "A high-altitude Himalayan resort town known for its cool climate and snow-capped mountains.",
+    img: "https://images.unsplash.com/photo-1597167231350-d057a45dc868?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWFuYWxpfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=500e",
+    whenToVisit: "Summer",
+    ratings: 4.7,
+    price: 10000,
   },
   {
-    id: 3,
-    name: "Goa Beaches",
-    description:
-      "Golden sandy beaches famous for vibrant nightlife and water sports along the Arabian Sea.",
-    imgUrl:
-      "https://images.unsplash.com/photo-1512343879784-a960bf40e7f2?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z29hJTIwYmVhY2hlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&q=60&w=700",
-    location: "Goa",
-    isUNESCO: false,
-    bestVisitSeason: ["November", "December", "January", "February"],
-    entryFee: 0,
+    placeName: "Kerala",
+    desc: "Known for its palm-lined beaches, backwaters, and rich traditions.",
+    img: "https://images.unsplash.com/photo-1602216056096-3b40cc0c9944?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1632",
+    whenToVisit: "Winter",
+    ratings: 4.8,
+    price: 20000,
   },
   {
-    id: 4,
-    name: "Golden Temple",
-    description:
-      "Holy Sikh shrine covered with gold and surrounded by a sacred pool in Amritsar.",
-    imgUrl:
-      "https://plus.unsplash.com/premium_photo-1697730324062-c012bc98eb13?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8R29sZGVuJTIwdGVtcGxlfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=700",
-    location: "Amritsar, Punjab",
-    isUNESCO: false,
-    bestVisitSeason: ["January", "February", "March", "April", "May"],
-    entryFee: 0,
-  },
-  {
-    id: 5,
-    name: "Manali",
-    description:
-      "Hill station known for breathtaking valleys, snowcapped mountains, and adventure sports.",
-    imgUrl:
-      "https://images.unsplash.com/photo-1549924231-f129b911e442?auto=format&fit=crop&w=800&q=80",
-    location: "Himachal Pradesh",
-    isUNESCO: false,
-    bestVisitSeason: [
-      "March",
-      "April",
-      "May",
-      "June",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-    entryFee: 0,
-  },
-  {
-    id: 6,
-    name: "Konark Sun Temple",
-    description:
-      "Magnificent 13th-century temple shaped like a chariot, a UNESCO World Heritage site.",
-    imgUrl:
-      "https://images.unsplash.com/photo-1749111728355-7a8824010fbe?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGtvbmFyayUyMHRlbXBsZXxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&q=60&w=700",
-    location: "Konark, Odisha",
-    isUNESCO: true,
-    bestVisitSeason: ["June", "July", "August", "September"],
-    entryFee: 30,
+    placeName: "Jaipur",
+    desc: "The capital of Rajasthan, known for its historic palaces and vibrant culture.",
+    img: "https://images.unsplash.com/photo-1477587458883-47145ed94245?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8amFpcHVyfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=500",
+    whenToVisit: "Winter",
+    ratings: 4.6,
+    price: 12000,
   },
 ];
 
@@ -98,15 +42,15 @@ function App() {
   return (
     <>
       <div className="flex flex-wrap max-w-[1000px] mx-auto">
-        {places.map((place) => {
+        {touristPlaces.map((place) => {
           return (
             <TouristPlaces
-              src={place.imgUrl}
-              placeName={place.name}
-              location={place.location}
-              desc={place.description}
-              visitingSeason={place.bestVisitSeason}
-              isUNESCO={place.isUNESCO}
+              src={place.img}
+              name={place.placeName}
+              desc={place.desc}
+              price={place.price}
+              rating={place.ratings}
+              weather={place.whenToVisit}
             />
           );
         })}
--- src/Components/TouristPlaces.jsx
+++ src/Components/TouristPlaces.jsx
@@ -1,43 +1,71 @@
-function TouristPlaces({
-  src,
-  placeName,
-  location,
-  desc,
-  visitingSeason,
-  isUNESCO,
-}) {
+function TouristPlaces({ src, name, desc, price, rating, weather }) {
   return (
     <>
       <div className="mx-auto">
-        <div className="border-2 rounded-lg border-neutral-300 px-2 pt-2 pb-5 w-[400px] m-6">
+        <div className="border-2 w-[400px] rounded-lg m-3 border-neutral-300 p-3 ">
           <div className="relative">
-            {/* <div
-              className="absolute hidden
-            {
-            let currentSeason = March;
-            } 
-             py-1 px-3 top-2 left-2 text-[14px] font-medium bg-amber-100 rounded-full text-amber-600"
-            >
-              visiting Season
-            </div> */}
-            <div
-              className="absolute hidden
-        
-             py-1 px-3 top-2 left-2 text-[14px] font-medium bg-indigo-100 rounded-full text-indigo-600"
-            >
-              UNESCO
+            <img
+              src={src}
+              className="rounded-lg h-60 w-full object-cover shadow-sm shadow-neutral-500"
+              alt=""
+            />
+
+            <div className="absolute px-4 py-1 rounded-full border border-green-100 text-white font-semibold top-2 left-2 text-sm bg-green-700">
+              {rating}
             </div>
 
-            <img className="rounded-lg" src={src} alt={placeName} />
-            <div className="absolute top-0 right-2 my-2 p-1 bg-green-100 w-15 rounded-full text-green-600 text-center">
-              <p className="font-medium text-[14px]">₹50</p>
+            <div
+              className={
+                price < 15000
+                  ? "absolute top-2 right-2 p-1 px-3 text-white font-semibold border border-amber-100 bg-amber-600 text-sm rounded-full"
+                  : "hidden"
+              }
+            >
+              Cheaper
             </div>
           </div>
 
-          <div>
-            <h1 className="text-[20px] font-semibold my-3">{placeName}</h1>
-            <p className="my-2 font-medium">{location}</p>
+          <div className="">
+            <h1 className="text-xl font-medium my-3">{name}</h1>
             <p className="">{desc}</p>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <div className="px-3 my-4 py-2 w-45  bottom-20 rounded-full font-semibold text-green-800 bg-green-100/80">
+              <p className="text-center">₹ {price} per person</p>
+            </div>
+            <div>
+              {weather == "Summer" ? (
+                <div className="text-yellow-700 font-medium bg-amber-100 px-3 py-2 flex gap-2 items-center rounded-full">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-brightness-high"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708" />
+                  </svg>
+                  <p>Summer</p>
+                </div>
+              ) : (
+                <div className="text-blue-700 font-medium bg-blue-100 px-3 py-2 flex gap-2 items-center rounded-full">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-snow3"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 7.5a.5.5 0 1 0 0 1 .5.5 0 0 0 0-1" />
+                    <path d="M8 16a.5.5 0 0 1-.5-.5v-1.293l-.646.647a.5.5 0 0 1-.707-.708L7.5 12.793v-1.51l-2.053-1.232-1.348.778-.495 1.85a.5.5 0 1 1-.966-.26l.237-.882-1.12.646a.5.5 0 0 1-.5-.866l1.12-.646-.883-.237a.5.5 0 1 1 .258-.966l1.85.495L5 9.155v-2.31l-1.4-.808-1.85.495a.5.5 0 1 1-.259-.966l.884-.237-1.12-.646a.5.5 0 0 1 .5-.866l1.12.646-.237-.883a.5.5 0 1 1 .966-.258l.495 1.849 1.348.778L7.5 4.717v-1.51L6.147 1.854a.5.5 0 1 1 .707-.708l.646.647V.5a.5.5 0 0 1 1 0v1.293l.647-.647a.5.5 0 1 1 .707.708L8.5 3.207v1.51l2.053 1.232 1.348-.778.495-1.85a.5.5 0 1 1 .966.26l-.236.882 1.12-.646a.5.5 0 0 1 .5.866l-1.12.646.883.237a.5.5 0 1 1-.26.966l-1.848-.495-1.4.808v2.31l1.4.808 1.849-.495a.5.5 0 1 1 .259.966l-.883.237 1.12.646a.5.5 0 0 1-.5.866l-1.12-.646.236.883a.5.5 0 1 1-.966.258l-.495-1.849-1.348-.778L8.5 11.283v1.51l1.354 1.353a.5.5 0 0 1-.707.708l-.647-.647V15.5a.5.5 0 0 1-.5.5m2-6.783V6.783l-2-1.2-2 1.2v2.434l2 1.2z" />
+                  </svg>
+                  <p>Winter</p>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
